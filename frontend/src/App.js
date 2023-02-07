c8205c89f7883d1cb0e6ba795d93aba0802c04fd
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-02-07 11:54:39 -0500

    crete modal for login and add some temporary css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage/LoginFormPage';
 import SignupFormPage from './components/SignupFormPage/SignupFormPage';
 import Navigation from './components/Navigation';
 
@@ -9,9 +8,6 @@ function App() {
     <>
       <Navigation />
       <Switch>
-        <Route path="/login">
-          <LoginFormPage />
-        </Route>
         <Route path="/signup">
           <SignupFormPage />
         </Route>
--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -1,17 +1,13 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
 import './LoginForm.css';
 
-function LoginFormPage() {
+function LoginForm() {
     const dispatch = useDispatch();
-    const sessionUser = useSelector(state => state.session.user);
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [errors, setErrors] = useState([]);
-
-    if (sessionUser) return ( <Redirect to="/" />);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -58,4 +54,4 @@ function LoginFormPage() {
     );
 }
 
-export default LoginFormPage;
+export default LoginForm;
