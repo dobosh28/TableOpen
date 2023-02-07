1b58959075faaad27dd9aeda1a20bfed5de0b568
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-02-06 21:49:11 -0500

    set up restoreSession thunk action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage';
+import LoginFormPage from './components/LoginFormPage/LoginFormPage';
 
 function App() {
   return (
     <Switch>
-      <Route path="/">
-        <LoginFormPage />
-      </Route>
+      <Route exact path="/login" component={LoginFormPage} />
     </Switch>
   );
 }
