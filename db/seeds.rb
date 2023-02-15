e09321e651dab0d7d80871d17834a691d3d40332
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-02-15 14:44:14 -0500

    edit seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,9 @@ require "open-uri"
 
 ApplicationRecord.transaction do
   puts "Destroying tables..."
+
+  Restaurant.destroy_all
+  Review.destroy_all
   User.destroy_all
 
   puts "Resetting primary keys..."
