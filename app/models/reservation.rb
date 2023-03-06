63f9f40bf0658b8a9e361e235d8260f53d3b8ce7
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-03-06 15:18:02 -0500

    edit user_id column in reservation table and add reservation model with validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,6 @@
 class Reservation < ApplicationRecord
+  validates_presence_of :restaurant_id, :party_size, :date, :time, :phone_number 
+
+  belongs_to :restaurant
+  belongs_to :user, optional: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_191343) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_201422) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_191343) do
   end
 
   create_table "reservations", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.integer "user_id"
     t.integer "restaurant_id", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
