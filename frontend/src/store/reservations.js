32660990bf7aa76e51c158d651533b0545237e27
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-03-13 14:57:54 -0400

    crate thunk action creators for reservations

--- frontend/src/store/reservations.js
+++ frontend/src/store/reservations.js
@@ -25,3 +25,55 @@ export const getReservation = (id) => (state) => {
   return state.reservations ? state.reservations[id] : null;
 };
 
+export const fetchReservations = () => async (dispatch) => {
+  const response = await fetch("/api/reservations");
+
+  if (response.ok) {
+    const reservations = await response.json();
+    dispatch(receiveReservations(reservations));
+  }
+};
+
+export const fetchReservation = (reservationId) => async (dispatch) => {
+  const response = await fetch(`/api/reservations/${reservationId}`);
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(receiveReservation(data.reservation));
+  }
+};
+
+export const createReservation = (reservation) => async (dispatch) => {
+  const response = await fetch("/api/reservations", {
+    method: "POST",
+    body: JSON.stringify(reservation),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(receiveReservation(data.reservation));
+  }
+};
+
+export const updateReservation = (reservation) => async (dispatch) => {
+  const response = await fetch(`/api/reservations/${reservation.id}`, {
+    method: "PUT",
+    body: JSON.stringify(reservation),
+  });
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(receiveReservation(data.reservation));
+  }
+};
+
+export const deleteReservation = (reservationId) => async (dispatch) => {
+  const response = await fetch(`/api/reservations/${reservationId}`, {
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+    dispatch(removeReservation(reservationId));
+  }
+};
+
