323325ea7d1adc56eaae25cfc8e5494430f1520b
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-03-13 14:52:16 -0400

    create reservations actions and action creators

---
+++ frontend/src/store/reservations.js
@@ -0,0 +1,27 @@
+const RECEIVE_RESERVATIONS = "reservations/RECEIVE_RESERVATIONS";
+const RECEIVE_RESERVATION = "reservations/RECEIVE_RESERVATION";
+const REMOVE_RESERVATION = "reservations/REMOVE_RESERVATION";
+
+const receiveReservations = (reservations) => ({
+  type: RECEIVE_RESERVATIONS,
+  reservations,
+});
+
+const receiveReservation = (reservation) => ({
+  type: RECEIVE_RESERVATION,
+  reservation,
+});
+
+const removeReservation = (reservationId) => ({
+  type: REMOVE_RESERVATION,
+  reservationId,
+});
+
+export const getReservations = (state) => {
+  return state.reservations ? Object.values(state.reservations) : [];
+};
+
+export const getReservation = (id) => (state) => {
+  return state.reservations ? state.reservations[id] : null;
+};
+
