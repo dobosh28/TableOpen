e90d849e9917fb2f12314c00ca242d4479e1ea39
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-03-27 23:43:42 -0400

    add styling to ReservationConfirmation

--- frontend/src/components/Reservations/ReservationConfirmation/ReservationConfirmation.js
+++ frontend/src/components/Reservations/ReservationConfirmation/ReservationConfirmation.js
@@ -1,17 +1,28 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getReservation, getReservations, fetchReservation } from '../../../store/reservations';
-import { useParams } from 'react-router-dom';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getReservation, fetchReservation } from "../../../store/reservations";
+import { useParams } from "react-router-dom";
+import "./ReservationConfirmation.css";
+import { getRestaurant } from "../../../store/restaurants";
 
 const ReservationConfirmation = () => {
   const dispatch = useDispatch();
   const { reservationId } = useParams();
   const reservation = useSelector(getReservation(reservationId));
-  const currentUser = useSelector(state => state.session.user);
+  const restaurantId = reservation?.restaurantId;
+  const restaurant = useSelector((state) => state.restaurants[restaurantId]);
 
+  
   useEffect(() => {
     dispatch(fetchReservation(reservationId));
-  }, [dispatch, reservationId]);
+  }, [dispatch, reservationId ]);
+
+  const allReservations = useSelector(state => Object.values(state.reservations));
+  console.log(allReservations);
+  
+
+  
+
 
   const formatDate = (dateString) => {
     const options = { weekday: "short", month: "short", day: "numeric" };
@@ -20,20 +31,37 @@ const ReservationConfirmation = () => {
   };
 
   return (
-  <div>
-    <h1>Reservation Confirmation</h1>
-    {reservation ? (
-      <div>
-        <h2>Party size: {reservation.partySize}</h2>
-        <h2>Date: {formatDate(reservation.date)}</h2>
-        <h2>Time: {reservation.time}</h2>
-      </div>
-    ) : (
-      <div>Loading...</div>
-    )}
-  </div>
-);
+    <>
+      <main className="res-confirmation-main">
+        <div className="res-confirmation-container">
+          <div className="res-confirmation-inner">
+            <div className="res-confirmation-holders">
+              <section className="res-confirmation-details">
+                <section className="res-confirmation-details-inner">
+                  <div className="confirmation-details-div">
+                    <div className="res-confirmation-restaurant-pic">
+                      <img src="" alt="" />
+                    </div>
+                  </div>
+                </section>
+              </section>
+            </div>
+          </div>
+        </div>
+      </main>
 
-}
+      <div>
+        <h1>Reservation Confirmation</h1>
+        {reservation && (
+          <div>
+            <h2>Party size: {reservation.partySize}</h2>
+            <h2>Date: {formatDate(reservation.date)}</h2>
+            <h2>Time: {reservation.time}</h2>
+          </div>
+        ) }
+      </div>
+    </>
+  );
+};
 
 export default ReservationConfirmation;
