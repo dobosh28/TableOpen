d8cd2eef69cb4e2bd0e24a778a85a767f2953019
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-04-12 00:59:52 -0400

    add restaurant additional info

--- frontend/src/components/RestaurantPage/RestaurantPage.js
+++ frontend/src/components/RestaurantPage/RestaurantPage.js
@@ -3,9 +3,10 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchRestaurant } from "../../store/restaurants";
 import { fetchReviews } from "../../store/reviews";
-import "./RestaurantPage.css";
 import ReviewsShow from "../Reviews/ReviewShow";
 import ReservationForm from "../Reservations/ReservationForm/ReservationForm";
+import AdditionalInfo from "./AdditionalInfo";
+import "./RestaurantPage.css";
 
 const RestaurantPage = () => {
   const dispatch = useDispatch();
@@ -23,6 +24,13 @@ const RestaurantPage = () => {
   }, [dispatch, restaurantId]);
 
   if (!restaurant) return null;
+
+  const getToReviews = () => {
+    window.scrollTo({
+      top: 1000,
+      behavior: "smooth",
+    });
+  };
 
   return (
     <div className="restaurant-page-main-container">
@@ -60,7 +68,9 @@ const RestaurantPage = () => {
                   <button className="nav-button">Menu</button>
                 </li>
                 <li className="nav-li">
-                  <button className="nav-button">Reviews</button>
+                  <button className="nav-button" onClick={getToReviews}>
+                    Reviews
+                  </button>
                 </li>
               </ol>
               <div></div>
@@ -110,6 +120,36 @@ const RestaurantPage = () => {
           <div className="reservation-form-takeout-box">
             <ReservationForm />
           </div>
+          <section className="additional-info">
+            <div>
+              <section className="restaurant-address">
+                <div>
+                  <span style={{ alignItems: "center", display: "flex" }}>
+                    <span
+                      style={{
+                        display: "inline-block",
+                        height: "1.5rem",
+                        width: "1.5rem",
+                        lineHeight: "1",
+                        minWidth: "1.5rem",
+                      }}
+                    >
+                      <svg viewBox="0 0 24 24" focusable="false">
+                        <g fill="none" fillRule="evenodd">
+                          <path
+                            d="M12,2 C16.418278,2 20,5.581722 20,10 C20,12.8133333 17.5666667,16.59 12.7,21.33 C12.3111565,21.7111429 11.6888435,21.7111429 11.3,21.33 C6.43333333,16.59 4,12.8133333 4,10 C4,5.581722 7.581722,2 12,2 Z M12,4 C8.6862915,4 6,6.6862915 6,10 C6,11.21 6.8,14 12,19.21 C17.2,14 18,11.21 18,10 C18,6.6862915 15.3137085,4 12,4 Z M12,7 C13.6568542,7 15,8.34314575 15,10 C15,11.6568542 13.6568542,13 12,13 C10.3431458,13 9,11.6568542 9,10 C9,8.34314575 10.3431458,7 12,7 Z M12,9 C11.4477153,9 11,9.44771525 11,10 C11,10.5522847 11.4477153,11 12,11 C12.5522847,11 13,10.5522847 13,10 C13,9.44771525 12.5522847,9 12,9 Z"
+                            fill="#2d333f"
+                          ></path>
+                        </g>
+                      </svg>
+                    </span>
+                    <p>{restaurant.address}</p>
+                  </span>
+                </div>
+              </section>
+              <AdditionalInfo restaurant={restaurant} />
+            </div>
+          </section>
         </div>
       </div>
     </div>
