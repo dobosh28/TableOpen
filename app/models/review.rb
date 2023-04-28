cec485a096b376906d6806d8c31d7437188045f1
Yosyp Dobosh <joseph.dobosh@gmail.com>
2023-04-28 17:30:45 -0400

    make edits in review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -19,7 +19,7 @@ class Review < ApplicationRecord
     validates_presence_of :nickname, :body, :overall, :food, :service, :ambience, :value, :user_id, :restaurant_id
     validates :overall, :food, :service, :ambience, :value, inclusion: { in: (1..5), message: "The rating must be between 1 and 5 stars" }
 
-    validates :body, length: { minimum: 50, maximum: 5000, message: "Must be between 50 and 5000 characters" }
+    validates :body, length: { minimum: 50, maximum: 2000, message: "Must be between 50 and 2000 characters" }
     validates :nickname, length: { minimum: 4, maximum: 24, message: "Must be between 4 and 24 characters" }
 
     belongs_to :user
